73ccefca19bb4654ba0b9e608dd020654cc2497e
Mateus Barros <mateusbarros2000@gmail.com>
2023-08-04 18:12:45 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotificationComponent } from './notification/notification.component';
+import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotificationComponent,
+    CabecalhoComponent
   ],
   imports: [
     BrowserModule,
